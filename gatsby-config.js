493b042be8dd4402d7ba962ff6bea4d10987255e
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-06-25 13:25:56 -0500

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,23 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
---
+++ src/pages/pandas-and-potatoes.md
@@ -0,0 +1,10 @@
+---
+title: "Pandas and Potatoes"
+date: "2017-10-10"
+---
+
+Pandas sort of like potatoes. 
+
+Here's a video of a panda, shamelessly stolen from another post and without any relevance.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4n0xNbfJLR8" frameborder="0" allowfullscreen></iframe>
